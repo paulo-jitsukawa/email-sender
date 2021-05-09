a966f41c501ae645cd0d23de98a37fc29958493b
Paulo Masaru Meneghel Jitsukawa <paulo.jitsukawa@edu.pucrs.br>
2021-05-09 10:45:42 -0300

    test: Monta projeto de testes de integração com autenticação

---
+++ src/Jitsukawa.EmailSender.Test/IntegrationTest.cs
@@ -0,0 +1,63 @@
+﻿using Jitsukawa.EmailSender.Api;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Jitsukawa.EmailSender.Test
+{
+    public class IntegrationTest : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        protected readonly WebApplicationFactory<Startup> factory;
+        public IntegrationTest(WebApplicationFactory<Startup> factory)
+        {
+            this.factory = factory;
+        }
+
+        protected HttpClient CreateClient(string apiKey)
+        {
+            var client = factory.CreateClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("ApiKey", apiKey);
+            return client;
+        }
+
+        protected async Task<(HttpStatusCode Code, string Response)> Get(string url, HttpClient client)
+        {
+            var result = await client.GetAsync(url);
+            var response = await result.Content.ReadAsStringAsync();
+
+            return (result.StatusCode, response);
+        }
+
+        protected async Task<(HttpStatusCode Code, string Response)> Post(string json, string url, HttpClient client)
+        {
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var result = await client.PostAsync(url, content);
+            var response = await result.Content.ReadAsStringAsync();
+
+            return (result.StatusCode, response);
+        }
+
+        protected async Task<(HttpStatusCode Code, string Response)> Put(string json, string url, HttpClient client)
+        {
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var result = await client.PutAsync(url, content);
+            var response = await result.Content.ReadAsStringAsync();
+
+            return (result.StatusCode, response);
+        }
+
+        protected async Task<(HttpStatusCode Code, string Response)> Delete(string url, HttpClient client)
+        {
+            var result = await client.DeleteAsync(url);
+            var response = await result.Content.ReadAsStringAsync();
+
+            return (result.StatusCode, response);
+        }
+    }
+}
---
+++ src/Jitsukawa.EmailSender.Test/WebAppFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Jitsukawa.EmailSender.Test
+{
+    public class WebAppFactory<TStartup> : WebApplicationFactory<TStartup> where TStartup : class
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                using (var scope = services.BuildServiceProvider().CreateScope())
+                {
+                    var scopedServices = scope.ServiceProvider;
+                    var logger = scopedServices.GetRequiredService<ILogger<WebAppFactory<TStartup>>>();
+                }
+            });
+        }
+    }
+}
